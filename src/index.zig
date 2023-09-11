9e4f1418f1f26ba0858ca3c12f64ade006fee5de
dman-os <61868957+dman-os@users.noreply.github.com>
2023-09-11 22:30:55 +0300

    feat(index): more BST methods

--- src/index.zig
+++ src/index.zig
@@ -23,13 +23,13 @@ pub fn BST(
     return struct {
         const Self = @This();
 
-        parent: ?*const Self = null,
+        parent: ?*Self = null,
         item: T,
         left_child: ?*Self = null,
         right_child: ?*Self = null,
 
         pub fn insert(self: *Self, cx: Ctx, in: *Self) void {
-            switch (cmp(cx, self.item, in.item)) {
+            switch (cmp(cx, in.item, self.item)) {
                 .lt => {
                     // println("node {} is adding {} to left", .{ self.item, in.item });
                     if (self.left_child) |child| {
@@ -51,8 +51,51 @@ pub fn BST(
             }
         }
 
+        pub fn transplant(self: *Self, in: ?*Self) void {
+            if (self.parent) |parent| {
+                if (self == parent.left_child) {
+                    parent.left_child = in;
+                } else {
+                    parent.right_child = in;
+                }
+                if (in) |incoming| {
+                    incoming.parent = parent;
+                }
+            }
+        }
+
+        pub fn delete(self: *Self) ?*Self {
+            defer self.parent = null;
+            defer self.right_child = null;
+            defer self.left_child = null;
+            if (self.left_child) |left| {
+                if (self.right_child) |right| {
+                    // find the successor which should be the min from
+                    // the right branch
+                    const succ = right.min();
+                    if (succ.parent != self) {
+                        // transplant it with its right child
+                        // NOTE: it doesn't have a left child since it's a min
+                        succ.transplant(succ.right_child);
+                        succ.right_child = right;
+                        right.parent = succ;
+                    }
+                    self.transplant(succ);
+                    succ.left_child = left;
+                    left.parent = succ;
+                    return succ;
+                } else {
+                    self.transplant(left);
+                    return left;
+                }
+            } else {
+                self.transplant(self.right_child);
+                return self.right_child orelse self.parent;
+            }
+        }
+
         pub fn find(self: *Self, cx: Ctx, val: T) ?*Self {
-            switch (cmp(cx, self.item, val)) {
+            switch (cmp(cx, val, self.item)) {
                 .eq => {
                     return self;
                 },
@@ -72,6 +115,64 @@ pub fn BST(
                     }
                 },
             }
+        }
+
+        pub fn min(self: *Self) *Self {
+            if (self.left_child) |child| {
+                return child.min();
+            } else {
+                return self;
+            }
+        }
+
+        pub fn max(self: *Self) *Self {
+            if (self.right_child) |child| {
+                return child.max();
+            } else {
+                return self;
+            }
+        }
+
+        pub fn predecessor(self: *Self) ?*Self {
+            if (self.left_child) |child| {
+                return child.max();
+            }
+            var x = self;
+            var y = self.parent;
+            while (true) {
+                if (y) |parent| {
+                    if (parent.left_child) |child| {
+                        if (x == child) {
+                            x = parent;
+                            y = parent.parent;
+                            continue;
+                        }
+                    }
+                }
+                break;
+            }
+            return y;
+        }
+
+        pub fn successor(self: *Self) ?*Self {
+            if (self.right_child) |child| {
+                return child.min();
+            }
+            var x = self;
+            var y = self.parent;
+            while (true) {
+                if (y) |parent| {
+                    if (parent.right_child) |child| {
+                        if (x == child) {
+                            x = parent;
+                            y = parent.parent;
+                            continue;
+                        }
+                    }
+                }
+                break;
+            }
+            return y;
         }
 
         pub fn initHeap(ha: Allocator, item: T) !*Self {
@@ -110,7 +211,7 @@ test "BST.insert" {
     );
     const ha = std.testing.allocator;
 
-    const tree = try MyBst.initHeap(ha, 500);
+    var tree = try MyBst.initHeap(ha, 500);
     defer MyBst.deinitHeap(ha, tree);
     const children = [_]usize{ 123, 53, 98823, 123, 534, 123, 54, 7264, 21, 0, 23 };
     for (children) |val| {
@@ -121,15 +222,37 @@ test "BST.insert" {
     try std.testing.expect(tree.find({}, 0) != null);
     try std.testing.expect(tree.find({}, 54) != null);
     try std.testing.expect(tree.find({}, 1223) == null);
+    try std.testing.expectEqual(@as(usize, 123), ((tree.find({}, 123) orelse unreachable).successor() orelse unreachable).item);
+    try std.testing.expectEqual(@as(usize, 54), ((tree.find({}, 123) orelse unreachable).predecessor() orelse unreachable).item);
+    try std.testing.expectEqual(@as(usize, 54), ((tree.find({}, 53) orelse unreachable).successor() orelse unreachable).item);
+    try std.testing.expectEqual(@as(usize, 53), ((tree.find({}, 54) orelse unreachable).predecessor() orelse unreachable).item);
+    try std.testing.expectEqual(@as(usize, 98823), tree.max().item);
+    try std.testing.expectEqual(@as(usize, 0), tree.min().item);
+    {
+        const find = tree.find({}, 23) orelse unreachable;
+        defer MyBst.deinitHeap(ha, find);
+        try std.testing.expectEqual(@as(usize, 21), (find.delete() orelse unreachable).item);
+    }
+    {
+        const find = tree;
+        defer MyBst.deinitHeap(ha, find);
+        tree = find.delete() orelse unreachable;
+        try std.testing.expectEqual(@as(usize, 534), tree.item);
+    }
+    {
+        const find = try MyBst.initHeap(ha, 500);
+        defer MyBst.deinitHeap(ha, find);
+        try std.testing.expect(find.delete() == null);
+    }
 }
-
+//
 // const BTreeConfig = struct {
-//     order: usize,
+//     order: usize = 12,
 // };
-
+//
 // fn BTree(
 //     comptime T: type,
-//     comptime order: usize,
+//     comptime config: BTreeConfig,
 // ) type {
 //      return struct {
 //         const Node = union(enum) {
